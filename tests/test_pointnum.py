36451399bd730f479d797562776da6dcde5a02f7
Keir <Keirhearn@gmail.com>
2021-06-25 14:15:34 +0100

    added test for point_find

--- tests/test_pointnum.py
+++ tests/test_pointnum.py
@@ -5,3 +5,10 @@ def test_pointnum1():
      pointnum = len(point_find((0,0),300,10))
      assert pointnum == 12
 
+def test_pointnum2():
+     pointnum = len(point_find((100,-100),400,25))
+     assert pointnum == 12
+
+def test_pointnum3():
+     pointnum = len(point_find((1000,-500),5000,50))
+     assert pointnum == 12
